b7e4205643d75a8f3548c2e6698f91b02d560018
Joao Vitor Chaves Guzela <joao.guzela@gmail.com>
2023-08-23 17:16:51 -0300

    Delete packages/backend/build directory

--- packages/backend/build/config/config.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.AUTHENTICATION_TOKEN = exports.NODE_PORT = void 0;
-exports.NODE_PORT = process.env.NODE_PORT || 3333;
-exports.AUTHENTICATION_TOKEN = 'guzela';
