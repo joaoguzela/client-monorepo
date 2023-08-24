28102b06512baaacfe66f142c610134217fd911e
joaoguzela <joao.guzela@gmail.com>
2023-08-24 03:03:26 -0300

    feat: paths backend

--- packages/navbar/vite.config.ts
+++ packages/navbar/vite.config.ts
@@ -1,15 +1,22 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-
+import path from 'path';
 // https://vitejs.dev/config/
 export default defineConfig({
   define: {
     'process.env': process.env,
   },
+  cacheDir: '../../node_modules/.vite',
+
   plugins: [react()],
   server: {
     host: true,
     strictPort: true,
     port: 5173,
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
   },
 });
