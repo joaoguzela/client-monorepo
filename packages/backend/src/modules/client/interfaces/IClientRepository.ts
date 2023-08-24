eaaa3ded41a03a7048c5434cb9294d04b87c6020
joaoguzela <joao.guzela@gmail.com>
2023-08-24 11:44:38 -0300

    refactor:Type orm version 0.3

--- packages/backend/src/modules/client/interfaces/IClientRepository.ts
+++ packages/backend/src/modules/client/interfaces/IClientRepository.ts
@@ -2,8 +2,8 @@ import { ClientType } from '../types/clientType';
 import Client from '../typeorm/entities/client';
 
 export default interface IClientRepository {
-  create(client: ClientType): Promise<Client | undefined>;
-  save(client: Client): Promise<Client | undefined>;
-  findById(id: string): Promise<Client | undefined>;
-  findByEmail(email: string): Promise<Client | undefined>;
+  create(client: ClientType): Promise<Client | null>;
+  save(client: Client): Promise<Client | null>;
+  findById(id: string): Promise<Client | null>;
+  findByEmail(email: string): Promise<Client | null>;
 }
--- packages/backend/wait-for-it.sh
+++ packages/backend/wait-for-it.sh
@@ -5,7 +5,7 @@ chmod +x wait-for-it.sh
 wait-for-it.sh apiclient-db:5432 -t 60
 
 yarn install
-yarn typeorm migration:run
+yarn typeorm -d src/typeorm/index.ts migration:run
 yarn dev
 
 
